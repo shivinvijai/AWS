8ba3ce08f50cc724460d9e18f48c6e5ba415b77c
Shivin Vijai <shivinvijai@gmail.com>
2017-12-24 00:29:37 +0530

    Some useful scripts using boto3

---
+++ boto3/s3_list.py
@@ -0,0 +1,9 @@
+import boto3
+# Lets use Amazon S3 bucket
+s3 = boto3.resource('s3')
+# List all buckets in this account
+count = 0
+for bucket in s3.buckets.all():
+    print(bucket.name)
+    count = count + 1
+print("Total bucket count is:", count)
